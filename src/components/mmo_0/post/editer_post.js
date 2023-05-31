49f4cbc20ca3c0d5ababfcd9e9c9b4ac20487110
vothanhdanh <999conan999@gmail.com>
2023-05-31 17:09:27 +0700

    vaasd

--- src/components/mmo_0/post/editer_post.js
+++ src/components/mmo_0/post/editer_post.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './post.css'
 // import { toast } from 'react-toastify';
 // import Template_input from '../lib/template_input/Template_input';
-import { Container,Grid,Button,Dropdown,Segment,Input,Image,Icon,Header,TextArea,Form } from 'semantic-ui-react'
+import { Container,Grid,Button,Dropdown,Segment,Input,Image,Checkbox,Header,TextArea,Form } from 'semantic-ui-react'
 export default class Editer_post extends Component {
   constructor (props) {
     super(props)
@@ -27,138 +27,202 @@ export default class Editer_post extends Component {
                 <Grid.Column width={12}>x</Grid.Column>
               </Grid> */}
               <Container>
-              <Header as='h1'>*Tạo bài viết mới</Header>
-                <div className='wrap-s'>
-                  <Grid>
-                    {/* <Grid.Column width={16}></Grid.Column> */}
-                    <Grid.Column width={4}>
-                      <Header as='h4'>*<span style={{color:"#03A9F4"}}>Bài viết</span> hay <span style={{color:"#03A9F4"}}>sản phẩm</span></Header>
-                      <Dropdown  selection
-                        value={this.state.selected_test}
-                        options={this.state.test}
-                        onChange={(e,{value}) => {
-                          this.setState({selected_test:value})
-                        }}
-                      />
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <Header as='h4'>*Chọn danh mục</Header>
-                      <Dropdown  selection
-                        value={this.state.selected_test}
-                        options={this.state.test}
-                        onChange={(e,{value}) => {
-                          this.setState({selected_test:value})
-                        }}
-                      />
-                    </Grid.Column>
-                    <Grid.Column width={4} >
-                      <Header as='h4'>*Chọn hình đại diện</Header>
-                      <button className='buzz re'
-                        //   onClick={()=>{
-                        //     let keyLock=makeid(6);
-                        //     this.setState({
-                        //         selected_img:{
-                        //             type:keyLock,
-                        //         }
-                        //     });
-                        //     this.props.openAction({
-                        //         type:"OPEN",
-                        //         is_muti_selected:false,
-                        //         keyLock:keyLock
-                        //     })
-                        // }}
-                      >
-                        <i className="fa-solid fa-photo-film"></i> <span>Add Media</span>
-                        <Image
-                          floated='right'
-                          size='tiny'
-                          src={'https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-Hoang-Gia-mau-HG02-300x300.jpg'}
-                          className='thuasda'
+                <Header as='h1'>*Tạo bài viết mới</Header>
+                {/* <Grid>
+                  <Grid.Column width={5}></Grid.Column>
+                  <Grid.Column width={6}>
+                      <div className='wrap-s'>
+                        <Header as='h4'>*Từ khóa Chính (cần SEO):</Header>
+                        <Input
+                            className="input-1"
+                            // label={{ icon: 'asterisk' }}
+                            // labelPosition='left corner'
+                            // placeholder='...'
+                            // value={text}
+                            // onChange={(e,{value}) => {
+                            //   this.props.fs_result(value)
+                            // }}
                         />
-                      </button>
+                      </div>
                     </Grid.Column>
-                    <Grid.Column width={4}>
-                      <Header as='h4'>*Thêm tag (nếu cần)</Header>
-                      <Dropdown
-                          options={this.state.test}
-                          placeholder='...'
-                          search
-                          selection
-                          fluid
-                          multiple
-                          allowAdditions
-                          value={this.state.selected_test_arr}
-                          onAddItem={(e, { value }) => {
-                            let {test}=this.state;
-                            test=[{ text: value, value }, ...test]
-                            this.setState({test:test})
-                          }}
-                          onChange={(e, { value }) => {
-                            this.setState({selected_test_arr:value})
-                          }}
-                        />
-                    </Grid.Column>
-                  </Grid>
-                </div>
-                
-                <div className='wrap-s'>
-                  <Grid>
-                      <Grid.Column width={10}>
-                        <Form>
-                            <Header as='h4'>*Tiêu đề bài viết</Header>
-                            <Input
-                                className="input-1"
-                                // label={{ icon: 'asterisk' }}
-                                // labelPosition='left corner'
-                                // placeholder='...'
-                                // value={text}
-                                // onChange={(e,{value}) => {
-                                //   this.props.fs_result(value)
-                                // }}
-                            />
-                        </Form>
-                      </Grid.Column>
-                      <Grid.Column width={6}>
-                        <Header as='h4'>*Chọn bảng giá</Header>
+                    <Grid.Column width={5}></Grid.Column>
+                  </Grid><br/> */}
+                  <div className='wrap-s'>
+                    <Grid>
+                      {/* <Grid.Column width={16}></Grid.Column> */}
+                      <Grid.Column width={4}>
+                        <Header as='h4'>*<span style={{color:"#03A9F4"}}>Bài viết</span> hay <span style={{color:"#03A9F4"}}>sản phẩm</span></Header>
                         <Dropdown  selection
-                        value={this.state.selected_test}
-                        options={this.state.test}
-                        onChange={(e,{value}) => {
-                          this.setState({selected_test:value})
-                        }}
-                      />
+                          value={this.state.selected_test}
+                          options={this.state.test}
+                          onChange={(e,{value}) => {
+                            this.setState({selected_test:value})
+                          }}
+                        />
                       </Grid.Column>
-                  </Grid>
-                </div>
-                <div className='wrap-s'>
-                  <Grid>
-                      <Grid.Column width={16}>
-                        <Form>
-                          <Header as='h4'>*Mô tả ngắn</Header>
-                          <TextArea placeholder='...' style={{ minHeight:80 }} 
+                      <Grid.Column width={4}>
+                        <Header as='h4'>*Chọn danh mục</Header>
+                        <Dropdown  selection
+                          value={this.state.selected_test}
+                          options={this.state.test}
+                          onChange={(e,{value}) => {
+                            this.setState({selected_test:value})
+                          }}
+                        />
+                      </Grid.Column>
+                      <Grid.Column width={4} >
+                        <Header as='h4'>*Chọn hình đại diện</Header>
+                        <button className='buzz re'
+                          //   onClick={()=>{
+                          //     let keyLock=makeid(6);
+                          //     this.setState({
+                          //         selected_img:{
+                          //             type:keyLock,
+                          //         }
+                          //     });
+                          //     this.props.openAction({
+                          //         type:"OPEN",
+                          //         is_muti_selected:false,
+                          //         keyLock:keyLock
+                          //     })
+                          // }}
+                        >
+                          <i className="fa-solid fa-photo-film"></i> <span>Add Media</span>
+                          <Image
+                            floated='right'
+                            size='tiny'
+                            src={'https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-Hoang-Gia-mau-HG02-300x300.jpg'}
+                            className='thuasda'
+                          />
+                        </button>
+                      </Grid.Column>
+                      <Grid.Column width={4}>
+                        <Header as='h4'>*Thêm tag (nếu cần)</Header>
+                        <Dropdown
+                            options={this.state.test}
+                            placeholder='...'
+                            search
+                            selection
+                            fluid
+                            multiple
+                            allowAdditions
+                            value={this.state.selected_test_arr}
+                            onAddItem={(e, { value }) => {
+                              let {test}=this.state;
+                              test=[{ text: value, value }, ...test]
+                              this.setState({test:test})
+                            }}
+                            onChange={(e, { value }) => {
+                              this.setState({selected_test_arr:value})
+                            }}
+                          />
+                      </Grid.Column>
+                    </Grid>
+                  </div>
+                  
+                  <div className='wrap-s'>
+                    <Grid>
+                        <Grid.Column width={4}>
+                          <Header as='h4'>*Từ khóa Chính (cần SEO):</Header>
+                          <Input
+                              className="input-1"
+                              // label={{ icon: 'asterisk' }}
+                              // labelPosition='left corner'
+                              // placeholder='...'
                               // value={text}
                               // onChange={(e,{value}) => {
                               //   this.props.fs_result(value)
                               // }}
+                          />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                          <Form>
+                              <Header as='h4'>*Tiêu đề bài viết</Header>
+                              <Input
+                                  className="input-1"
+                                  // label={{ icon: 'asterisk' }}
+                                  // labelPosition='left corner'
+                                  // placeholder='...'
+                                  // value={text}
+                                  // onChange={(e,{value}) => {
+                                  //   this.props.fs_result(value)
+                                  // }}
+                              />
+                          </Form>
+                        </Grid.Column>
+                        <Grid.Column width={4}>
+                          <Header as='h4' className='re'>*Chọn bảng giá: 
+                            <Image
+                              size='tiny'
+                              src={'https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-Hoang-Gia-mau-HG02-300x300.jpg'}
+                              className='tablker'
                             />
-                        </Form>
-                      </Grid.Column>
-                      
-                  </Grid>
-                </div>
-                <Grid>
-                    <Grid.Column width={10}>
-                    <div className='wrap-s'>
-xxx
-                    </div>
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                    <div className='wrap-s'>
-xxx
-                    </div>
-                    </Grid.Column>
-                </Grid>
-
+                          </Header>
+                          <Dropdown  selection
+                          value={this.state.selected_test}
+                          options={this.state.test}
+                          onChange={(e,{value}) => {
+                            this.setState({selected_test:value})
+                          }}
+                        />
+                        </Grid.Column>
+                    </Grid>
+                  </div>
+                  <div className='wrap-s'>
+                    <Grid>
+                        <Grid.Column width={16}>
+                          <Form>
+                            <Header as='h4'>*Mô tả ngắn</Header>
+                            <TextArea placeholder='...' style={{ minHeight:80 }} 
+                                // value={text}
+                                // onChange={(e,{value}) => {
+                                //   this.props.fs_result(value)
+                                // }}
+                              />
+                          </Form>
+                        </Grid.Column>
+                        
+                    </Grid>
+                  </div>
+                  <Header as='h1' textAlign="center">*Nội dung bài viết</Header>
+                  <div>
+                    <Segment>
+                      <Grid>
+                          <Grid.Column width={10}>
+                          <div className='wrap-x'>
+      xxx
+                          </div>
+                          </Grid.Column>
+                          <Grid.Column width={6}>
+                          <div className='wrap-s'>
+                            <div>
+                              <span> <b style={{marginRight:"16px"}}>Chọn thuộc tính:</b>
+                                <Dropdown  selection
+                                  value={this.state.selected_test}
+                                  options={this.state.test}
+                                  onChange={(e,{value}) => {
+                                    this.setState({selected_test:value})
+                                  }}
+                                />
+                              </span>
+                            </div>
+                            <div className='show-rsx'>
+                              <span>Giá gốc: <ins>1.260.0000đ</ins></span><br/>
+                              <span>Giá khuyến mãi: <ins>1.060.0000đ</ins></span><br/>
+                              <span>Thuộc tính: <ins>1m2 x dài 2m</ins></span><br/>
+                              <span>Khuyến mãi: <ins>Có</ins></span>
+                            </div>
+                            <Input label='Ẩn content' placeholder='50px || auto || ""' className="input-1"  />
+                            <span className="op">Chỉnh sửa mô tả</span>
+                            <Checkbox toggle label='Hiển thị nút mua sản phẩm' className='mgt-8' checked />
+                            <div className='mgt-8'>
+                            <p class="tyas">Xóa hình ảnh và nội dung này</p>
+                            </div>
+                          </div>
+                          </Grid.Column>
+                      </Grid>
+                    </Segment>
+                  </div>
               </Container>
               
               <div className='footer-edit'>
--- src/components/mmo_0/post/posts.js
+++ src/components/mmo_0/post/posts.js
@@ -11,19 +11,19 @@ export default class Posts extends Component {
       // data:[],
         test:[
           { text: 'Hiển thị tất cả danh mục', value: 'English' },
-          {text: 'French', value: 'French' },
-          { text: 'Spanish', value: 'Spanish' },
-          {text: 'German', value: 'German' },
+          {text: 'Giường sắt', value: 'French' },
+          { text: 'GIường ngủ', value: 'Spanish' },
+          {text: 'giường gỗ', value: 'German' },
           { text: 'Chinese', value: 'Chinese' },
         ],
-        selected_test:'English'
+        selected_test:'English',
+        selected_test_arr:[]
       
     }
   }
   render() {
       return (
         <React.Fragment>
-          <Container>
               <Grid>
                 <Grid.Column width={4}>
                   <div className='tao-moi-post '>
@@ -59,20 +59,54 @@ export default class Posts extends Component {
                       <Table.Row>
                         <Table.HeaderCell width={1} className='idzx'>id <Input transparent placeholder='Search...' size='mini' type='number'/></Table.HeaderCell>
                         <Table.HeaderCell width={1}>Thumnail</Table.HeaderCell>
-                        <Table.HeaderCell width={6}>Tiêu đề: <Input transparent placeholder='Search...' size='mini' /></Table.HeaderCell>
-                        <Table.HeaderCell width={2}>Trạng thái <Button size='mini'>All</Button> </Table.HeaderCell>
+                        <Table.HeaderCell width={4}>Tiêu đề: <Input transparent placeholder='Search...' size='mini' /></Table.HeaderCell>
+                        <Table.HeaderCell width={1}>Type  <Button size='mini' basic icon="sort amount down"/></Table.HeaderCell>
+                        <Table.HeaderCell width={3}>Link to KeyWord</Table.HeaderCell>
+                        <Table.HeaderCell width={2}>Trạng thái <Button size='mini' basic >All</Button> </Table.HeaderCell>
                         <Table.HeaderCell width={4}>Điều chỉnh</Table.HeaderCell>
                         <Table.HeaderCell width={1}>Active</Table.HeaderCell>
                       </Table.Row>
                     </Table.Header>
 
                     <Table.Body>
-                      <Table.Row className='active-da'>
+                      <Table.Row className='todo'>
+                      {/* <Table.Row className='active-da'> */}
                         <Table.Cell>176</Table.Cell>
                         <Table.Cell textAlign='middle'>
                           <Image src='https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-Hoang-Gia-mau-HG02-300x300.jpg' className='imgthm'/>
                         </Table.Cell>
                         <Table.Cell>None</Table.Cell>
+                        <Table.Cell>
+                          <a className='tagx'>S.P</a>
+                          <a className='tagx colrfs'>B.V</a>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <div><b>Giường sắt ống tròn </b> (3) 
+                            <Button  icon='edit' basic className='border-non' />
+                          </div>
+                          {false&&<div className='re'>
+                            <div className='keyworsx'>
+                             <p><b>*Chọn từ khóa cần link tới (Lưu ý: chỉ chọn được <span style={{color:"blue"}}>3 từ</span>)</b></p>
+                             <p><b>*Từ khóa chính hiện tại <span style={{color:"blue"}}>Giường sắt ống tròn</span>)</b></p>
+                              <Dropdown
+                                search
+                                options={this.state.test}
+                                placeholder='...'
+                                multiple
+                                selection
+                                fluid
+                                value={this.state.selected_test_arr}
+                                onChange={(e, { value }) => {
+                                  this.setState({selected_test_arr:value})
+                                }}
+                              />
+                              <div className='huhvx'>
+                                  <Button content='Primary' primary />
+                                  <Button content='Secondary' secondary />
+                              </div>
+                            </div>
+                          </div>}
+                        </Table.Cell>
                         <Table.Cell>
                           <Button content='Riêng tư' basic size="mini" />
                           {/* <Button positive  size="mini">Công khai</Button> */}
@@ -103,44 +137,13 @@ export default class Posts extends Component {
                         </Table.Cell>
                       </Table.Row>
                       
-                      <Table.Row>
-                        <Table.Cell>176</Table.Cell>
-                        <Table.Cell textAlign='middle'>
-                          <Image src='https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-Hoang-Gia-mau-HG02-300x300.jpg' className='imgthm'/>
-                        </Table.Cell>
-                        <Table.Cell>None</Table.Cell>
-                        <Table.Cell>
-                          <Button content='Riêng tư' basic size="mini" />
-                          {/* <Button positive  size="mini">Công khai</Button> */}
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Button animated='vertical'>
-                            <Button.Content hidden >Xóa</Button.Content>
-                            <Button.Content visible>
-                              <Icon name='trash alternate' />
-                            </Button.Content>
-                          </Button>
-                          <Button animated='vertical'>
-                            <Button.Content hidden>Copy</Button.Content>
-                            <Button.Content visible>
-                              <Icon name='copy' />
-                            </Button.Content>
-                          </Button>
-                          <Button animated='vertical'>
-                            <Button.Content hidden>Edit</Button.Content>
-                            <Button.Content visible>
-                              <Icon name='edit' />
-                            </Button.Content>
-                          </Button>
-                        </Table.Cell>
-                        <Table.Cell><Button circular icon='quidditch' size='big' color='gray'/></Table.Cell>
-                      </Table.Row>
+                       
                     
                     </Table.Body>
 
                     <Table.Footer  className='foot-tbaks'>
                       <Table.Row>
-                        <Table.HeaderCell colSpan='5'>
+                        <Table.HeaderCell colSpan='7'>
                           <div style={{textAlign:"center"}}>xxx</div>
                         </Table.HeaderCell>
                       </Table.Row>
@@ -148,8 +151,8 @@ export default class Posts extends Component {
                   </Table>
                 </Segment>
               </Grid.Column>
-          </Container>
               <Editer_post/>
+              {false&&<div className='dimerz'></div>}
         </React.Fragment>
       );
   }
